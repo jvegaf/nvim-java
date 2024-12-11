202592418e1d9e888fb47cb6f2d593cc21c30e19
Jose Vega <josevega234@gmail.com>
2024-12-11 12:29:33 +0100

    keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -31,6 +31,7 @@ keymap.set("n", "<leader>to", ":tabnew<CR>") -- open a new tab
 keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close a tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") -- next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") -- previous tab
+keymap.set("n", "<leader>,", ":tabNext<CR>")
 
 -- Diff keymaps
 keymap.set("n", "<leader>cc", ":diffput<CR>") -- put diff from current to other during diff
