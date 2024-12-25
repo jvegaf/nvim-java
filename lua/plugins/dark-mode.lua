b48086ca853b29b548077391fbb5d2d987eca1e1
Jose Vega <josevega234@gmail.com>
2024-12-25 04:28:14 +0100

    fixes

--- lua/plugins/dark-mode.lua
+++ lua/plugins/dark-mode.lua
@@ -5,7 +5,8 @@ return {
     update_interval = 1000,
     set_dark_mode = function()
       vim.api.nvim_set_option_value("background", "dark", {})
-      vim.cmd("colorscheme tokyonight-night")
+      -- vim.cmd("colorscheme tokyonight-night")
+      vim.cmd("colorscheme monokai")
     end,
     set_light_mode = function()
       vim.api.nvim_set_option_value("background", "light", {})
