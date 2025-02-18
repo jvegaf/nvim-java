5b2d9399b8ff91def2b65ed192c870f2f044bc8e
Jose Vega <josevega234@gmail.com>
2025-02-18 22:34:33 +0100

    py

--- lua/plugins/lang/python.lua
+++ lua/plugins/lang/python.lua
@@ -1,7 +1,7 @@
 return {
   {
     'jim-at-jibba/micropython.nvim',
-    -- enabled = false,
+    enabled = false,
     event = 'VeryLazy',
     dependencies = { 'akinsho/toggleterm.nvim', 'stevearc/dressing.nvim' },
     config = function()
