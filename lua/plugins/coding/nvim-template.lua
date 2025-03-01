f42130c359c89b940b605dc2da6b463b1bb8f813
Jose Vega <josevega234@gmail.com>
2025-03-01 10:54:05 +0100

    templ

--- lua/plugins/coding/nvim-template.lua
+++ lua/plugins/coding/nvim-template.lua
@@ -1,18 +1,43 @@
 return {
-  'glepnir/template.nvim',
-  cmd = { 'Template', 'TemProject' },
-  dependencies = {
-    'nvim-telescope/telescope.nvim',
+  {
+    'glepnir/template.nvim',
+    enabled = false,
+    cmd = { 'Template', 'TemProject' },
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+    },
+    config = function()
+      require('template').setup({
+        temp_dir = '~/.config/nvim/templates',
+        -- config in there
+      })
+      require('telescope').load_extension('find_template')
+    end,
+    keys = {
+      { '<leader>ct', ':Template<cr>', desc = 'Create a template file' },
+      {
+        '<leader>ft',
+        function()
+          require('telescope').extensions.find_template({ type = 'insert' })
+        end,
+        desc = 'Find File Template',
+      },
+    },
   },
-  config = function()
-    require('template').setup({
-      temp_dir = '~/.config/nvim/templates',
-      -- config in there
-    })
-    require('telescope').load_extension('find_template')
-  end,
-  keys = {
-    { '<leader>ct', ':Template<cr>', desc = 'Create a template file' },
-    { '<leader>ft', ':Telecope find_template type=insert<cr>', desc = 'Find File Template' },
+  {
+    'cvigilv/esqueleto.nvim',
+    event = 'VeryLazy',
+    branch = 'develop',
+    opts = {
+      directories = {
+        vim.fn.stdpath('config') .. '/templates',
+      },
+      patterns = {
+        -- Filetype
+        'cpp',
+        'js',
+        'lua',
+      },
+    },
   },
 }
