30576f9450dc950dcdabcd4b39c899ee5d73314f
Jose Vega <josevega234@gmail.com>
2025-03-10 19:06:14 +0100

    js ft

--- ftplugin/javascriptreact.lua
+++ ftplugin/javascriptreact.lua
@@ -13,3 +13,21 @@ wk.add({
   { "<leader>Cg", "<cmd>VtsExec goto_source_definition<Cr>", "GoToSourceDefinition" },
   { "<leader>CR", "<cmd>VtsExec file_references<Cr>", "Find File References" },
 })
+
+
+  -- Automatically end a self-closing tag when pressing /
+vim.keymap.set("i", "/", function()
+  local node = vim.treesitter.get_node()
+  if not node then
+    return "/"
+  end
+
+  if node:type() == "jsx_opening_element" then
+    local char_at_cursor = vim.fn.strcharpart(vim.fn.strpart(vim.fn.getline("."), vim.fn.col(".") - 2), 0, 1)
+    local already_have_space = char_at_cursor == " "
+
+    return already_have_space and "/>" or " />"
+  end
+
+  return "/"
+end, { expr = true, buffer = true }))
